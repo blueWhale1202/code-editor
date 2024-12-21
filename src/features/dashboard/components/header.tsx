7ff7d1055735bafed8debe411584c5a7a37e2c1c
mrfour4 <21521609@gm.uit.edu.com>
2024-12-22 01:26:38 +0700

    chore: add separator

--- src/features/dashboard/components/header.tsx
+++ src/features/dashboard/components/header.tsx
@@ -4,6 +4,7 @@ import { Logo } from "@/components/logo";
 import { Navigation } from "@/components/navigation";
 import { ProfileButton } from "@/components/profile-button";
 
+import { Separator } from "@/components/ui/separator";
 import { LanguageSelector } from "./language-selector";
 import { RunButton } from "./run-button";
 import { ThemeSelector } from "./theme-selector";
@@ -14,23 +15,23 @@ export const Header = () => {
             <div className="mb-4 flex items-center justify-center rounded-lg bg-[#0a0a0f]/80 p-6 backdrop-blur-xl lg:justify-between">
                 <div className="hidden items-center justify-start gap-8 lg:flex">
                     <Logo />
-
                     <Navigation />
                 </div>
 
-                <div className="flex items-center gap-4">
-                    <div className="flex items-center gap-3">
-                        <ThemeSelector />
-                        <LanguageSelector />
-                    </div>
+                <div className="flex items-center gap-3">
+                    <ThemeSelector />
+                    <LanguageSelector />
 
                     <SignedIn>
                         <RunButton />
                     </SignedIn>
 
-                    <div className="flex items-center gap-2 border-l border-gray-800 pl-3">
-                        <ProfileButton />
-                    </div>
+                    <Separator
+                        orientation="vertical"
+                        className="mx-1 h-8 w-0.5"
+                    />
+
+                    <ProfileButton />
                 </div>
             </div>
         </header>
